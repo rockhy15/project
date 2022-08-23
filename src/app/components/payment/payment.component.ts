fea8c3745825d277f70910de7e4abff6999ebb6b
rockhy15 <rakeshmichael2000@gmail.com>
2022-08-23 18:06:50 +0530

    angular project

---
+++ src/app/components/payment/payment.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.css']
+})
+export class PaymentComponent implements OnInit {
+
+  constructor() { }
+
+  public mss:boolean =false;
+  public mss:boolean =false;
+
+  
+  ngOnInit(): void {
+  }
+
+  creation(){
+    this.mss = true;
+
+  }
+  fun(){
+    this.mss =true;
+  }
+}
+
